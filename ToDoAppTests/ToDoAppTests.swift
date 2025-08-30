acadfdcd4dd7e7956187009dcd5e172114f44117
DaveC <Dave90807@msn.com>
2025-08-30 10:03:43 -0700

    Initial Commit

---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Dave Cassidy on 8/30/25.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
