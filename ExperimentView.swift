f11be7a50183e7bcef3229b2c53ddb72e7eff7e0
DaveC <Dave90807@msn.com>
2025-08-31 16:03:05 -0700

    Completed Task 5: "To-Do List with add, toggle, and navigation."

---
+++ ExperimentView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperimentView.swift
+//  ToDoApp
+//
+//  Created by Dave Cassidy on 8/31/25.
+//
+
+import SwiftUI
+
+struct ExperimentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperimentView()
+}
---
+++ Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoApp
+//
+//  Created by Dave Cassidy on 8/30/25.
+//
+
+import Foundation
